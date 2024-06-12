95f67886cb6069df8176e3099d347d22246af3b1
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-06-12 11:26:14 -0400

    Starter code for mutations

--- client/src/AppRecipeApi.js
+++ client/src/AppRecipeApi.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Recipe from './components/Recipe';
+import Recipe from './components/RecipeApi';
 
 const App = () => {
     const [recipes, setRecipes] = useState([]);
--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const { application } = require('express');
 
 const getExternalData = async () => {
   try {
