3dc5583e25417555a39d3e7bbeba99c2ef86dfaa
sjastrow <skjastrow@gmail.com>
2024-06-12 00:01:04 -0400

    updated header and footer.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import YardIcon from "@mui/icons-material/Yard";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{
+          backgroundColor: "#384e1d",
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <YardIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Root2Roast
+          </Typography>
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+          <Button color="inherit" component={Link} to="/signup">
+            Sign Up
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
