e65619ad26037f2e4ea4293b60d871df68828fec
sjastrow <skjastrow@gmail.com>
2024-06-15 17:29:40 -0400

    Hook up api to ingredient form and display new ingredient cards

--- client/src/components/Ingredientform.jsx
+++ client/src/components/Ingredientform.jsx
@@ -1,26 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { Box, Typography, TextField, Button } from "@mui/material";
+import { Box, Typography, TextField, Button, Grid } from "@mui/material";
 import axios from "axios";
+import IngredientResultCard from "./IngredientResultCard";
+
 export const Ingredientform = () => {
   const [data, setData] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
   const [query, setQuery] = useState("");
   const [error, setError] = useState(null);
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // Perform form submission logic here (e.g., send data to server)
-  };
 
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await axios.get(
-          `https://api.edamam.com/api/recipes/v2?type=user&q=tomato&app_id=b51976f6&app_key=%204b58f8200ceb9dd0a25df2ef3bd593af%09
+          `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=b51976f6&app_key=%204b58f8200ceb9dd0a25df2ef3bd593af%09
 `
         );
         console.log(
-          `https://api.edamam.com/api/recipes/v2?type=user&q=tomato&app_id=b51976f6&app_key=%204b58f8200ceb9dd0a25df2ef3bd593af%09
+          `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=b51976f6&app_key=%204b58f8200ceb9dd0a25df2ef3bd593af%09
 `
         );
         setData(response.data);
@@ -51,41 +48,48 @@ export const Ingredientform = () => {
       sx={{
         backgroundColor: "rgba(0, 0, 0, 0.3)",
         height: "80vh",
-        bgcolor: "background.paper",
         p: 2,
         boxShadow: 3,
         overflow: "scroll",
       }}
     >
-      <Typography
-        variant="h1"
-        gutterBottom
-        sx={{ color: "#black", fontSize: "45px" }}
-      >
-        Ingredient form
-      </Typography>
-      <Typography
-        variant="body1"
-        gutterBottom
-        sx={{ color: "#fff", width: "100%" }}
-      >
-        List a ingredient below.
-      </Typography>
       <form onSubmit={handleSearchSubmit}>
-        <TextField
-          label="Ingredient"
-          variant="filled"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          sx={{ width: "50%" }}
-          required
-        />
-        <Button type="submit" variant="contained">
-          Submit
-        </Button>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          mb={3}
+        >
+          <TextField
+            label="Enter ingredient here"
+            variant="filled"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            sx={{ width: "70%", backgroundColor: "#fff" }}
+            required
+          />
+          <Button type="submit" variant="contained">
+            Submit
+          </Button>
+        </Box>
       </form>
       {error && <p style={{ color: "red" }}>Error fetching data: {error}</p>}
-      <p>{JSON.stringify(data, null, 2)}</p>
+      {data && (
+        <Grid container spacing={2}>
+          {data?.hits?.map((item, index) => {
+            return (
+              <Grid item xs={12}>
+                <IngredientResultCard
+                  title={item?.recipe?.label}
+                  img={item?.recipe?.image}
+                  ingredients={item?.recipe?.ingredientLines}
+                  url={item?.recipe?.shareAs}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
     </Box>
   );
 };
